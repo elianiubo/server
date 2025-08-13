6cc5a29347d90bbce9655af07443345e3b8967c3
Elia <elia.niubo@gmail.com>
2025-08-13 19:43:04 +0200

    db

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL_PG,
+  connectionString: process.env.DATABASE_URL,
 //   ssl: { rejectUnauthorized: false },
 });
 
