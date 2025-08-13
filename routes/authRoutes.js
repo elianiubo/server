3ff87d60301afaf2721e122fe6c2d7ad1376b35c
Elia <elia.niubo@gmail.com>
2025-08-13 20:03:25 +0200

    auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,22 +1,18 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const router = express.Router();
 
 // ⚠️ Email/contraseña hardcoded para este ejemplo simple
-// const ADMIN_EMAIL = "admin@ejemplo.com";
-// const ADMIN_PASSWORD = "123456"; // En producción, usa bcrypt y una base de datos
-router.get("/admin", verifyToken, (req, res) => {
-  res.json({ message: `Bienvenido, ${req.user.email}` });
-});
+const ADMIN_EMAIL = "admin@ejemplo.com";
+const ADMIN_PASSWORD = "123456"; // En producción, usa bcrypt y una base de datos
 
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
 
-  if (email !== process.env.ADMIN_EMAIL || password !== process.env.ADMIN_PASSWORD) {
+  if (email !== ADMIN_EMAIL || password !== ADMIN_PASSWORD) {
     return res.status(401).json({ error: "Credenciales incorrectas" });
   }
 
