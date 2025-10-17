9363bb286e72060ea837275c367a8c5c85823ac0
Elia <elia.niubo@gmail.com>
2025-10-17 19:06:53 +0200

    fixed login error and others

--- config/uploadToCloudinary.js
+++ config/uploadToCloudinary.js
@@ -3,7 +3,13 @@ async function uploadToCloudinary(file) {
   formData.append("file", file);
   formData.append("upload_preset", "YOUR_UNSIGNED_PRESET"); // desde Cloudinary
   formData.append("folder", "portfolio"); // opcional
+  const MAX_MB = 8;
+  const ALLOWED = ["image/jpeg", "image/png", "image/webp", 'image/jpg'];
 
+  for (const file of files) {
+    if (!ALLOWED.includes(file.type)) throw new Error("Formato no permitido");
+    if (file.size > MAX_MB * 1024 * 1024) throw new Error("Archivo demasiado grande");
+  }
   const response = await fetch("https://api.cloudinary.com/v1_1/dcgj0tvya/image/upload", {
     method: "POST",
     body: formData
